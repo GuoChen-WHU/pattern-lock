e3707282941bd9b673c731624ee4fe8b30869716
GuoChen-WHU <496474070@qq.com>
2017-05-16 20:19:54 +0800

    improve performance

--- pattern-lock.js
+++ pattern-lock.js
@@ -6,7 +6,7 @@
 
   if (typeof $ === 'undefined') throw new Error('Pattern lock need jquery as a dependency.');
 
-  var PatternLock = function (container, options) {
+  let PatternLock = function (container, options) {
     this.$container = $(container);
     this.$container.css('position') === 'static' && this.$container.css('position', 'relative');
     this.options = options;
@@ -46,6 +46,8 @@
     this.lineCtx = lineCanvas.getContext('2d');
     this.elasticCtx = elasticCanvas.getContext('2d');
 
+    // 待搜索的圆
+    this.searchList = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     // 当前选中的圆
     this.currentCircle = -1;
     // 存放滑过的圆索引，触摸结束时即手势结果
@@ -163,7 +165,7 @@
    * 页面坐标转换为canvas坐标
    */
   PatternLock.prototype.windowToCanvas = function (x, y) {
-    var bbox = this.$container[0].getBoundingClientRect();
+    let bbox = this.$container[0].getBoundingClientRect();
     return {
       x: x - bbox.left,
       y: y - bbox.top
@@ -184,15 +186,15 @@
    * 根据触摸位置获取对应的圆
    */
   PatternLock.prototype.getTargetCircle = function (pos) {
-    // TODO:改用效率更高的搜索算法
-    var centers = this.options.centers,
+    let centers = this.options.centers,
         radius = this.options.circleRadius;
-    for (var i = 0; i < 9; i++) {
-      if (pos.x < centers[i][0] + radius &&
-          pos.x > centers[i][0] - radius &&
-          pos.y < centers[i][1] + radius &&
-          pos.y > centers[i][1] - radius)
-        return i;
+    for (let i = 0, len = this.searchList.length; i < len; i++) {
+      let num = this.searchList[i];
+      if ((pos.x - centers[num][0]) * (pos.x - centers[num][0]) + 
+          (pos.y - centers[num][1]) * (pos.y - centers[num][1]) < radius * radius) {
+            this.searchList.splice(i, 1);
+            return num;
+          }
     }
   };
 
@@ -200,7 +202,7 @@
    * 更新橡皮筋
    */
   PatternLock.prototype.updateElastic = function (pos) {
-    var centers = this.options.centers;
+    let centers = this.options.centers;
     this.elasticCtx.clearRect(0, 0, this.width, this.height);
     this.drawLine(
       this.elasticCtx,
@@ -217,14 +219,12 @@
   PatternLock.prototype.onMouseDownOrTouchStart = function (e) {
     e.preventDefault();
 
-    var coor = this.getEvtPos(e),
+    let coor = this.getEvtPos(e),
         pos = this.getStdPos(this.windowToCanvas(coor.x, coor.y)),
         index = this.getTargetCircle(pos),
         centers = this.options.centers,
         radius = this.options.pointRadius;
 
-    // 如果选中了圆，记录该圆的索引，改变它的样式，监听触摸滑动和触摸结束事件，
-    // 保存当前绘图
     if (index !== void 0) {
       this.currentCircle = index;
       this.result.push(index);
@@ -244,26 +244,17 @@
   PatternLock.prototype.onMouseMoveOrTouchMove = function (e) {
     e.preventDefault();
 
-    var coor = this.getEvtPos(e),
+    let coor = this.getEvtPos(e),
         pos = this.getStdPos(this.windowToCanvas(coor.x, coor.y)),
         index = this.getTargetCircle(pos),
         centers = this.options.centers,
         radius = this.options.pointRadius;
 
-    // 滑到了一个圆处，需要选中该圆，改变橡皮筋起点
     if (index !== void 0) {
-      // 已经记录过的圆，直接返回
-      if (this.result.indexOf(index) > -1) return;
-      // 画线
       this.drawLine(this.lineCtx, centers[this.currentCircle][0], centers[this.currentCircle][1], centers[index][0], centers[index][1]);
-
-      // 该圆存入结果中
-      this.result.push(index);
-
-      // 高亮该圆
-      this.activateCircle(index);
-      // 改变橡皮筋起点
       this.currentCircle = index;
+      this.result.push(index);
+      this.activateCircle(index);
       // 清除橡皮筋
       this.elasticCtx.clearRect(0, 0, this.width, this.height);
       return;
@@ -281,16 +272,16 @@
     this.$container.off('mousemove');
     this.$container.off('mouseup');
 
-    var password = this.result.join('');
+    let password = this.result.join('');
     // 设置模式，记录密码，进入'again'状态
     if (this.options.mode === 'set') {
       // 验证密码长度
       if (password.length < this.options.minLength) {
-        var shortEvent = $.Event('short.patternlock', {shortPassword: password});
+        let shortEvent = $.Event('short.patternlock', {shortPassword: password});
         this.$container.trigger(shortEvent);
       } else {
         this.initPassword = password;
-        var initEvent = $.Event('init.patternlock', {initPassword: password});
+        let initEvent = $.Event('init.patternlock', {initPassword: password});
         this.$container.trigger(initEvent);
         this.options.mode = 'again';
       }
@@ -299,11 +290,11 @@
     } else if (this.options.mode === 'again') {
       // 密码不一致
       if (this.initPassword !== password) {
-        var diffEvent = $.Event('diff.patternlock', {diffPassword: password});
+        let diffEvent = $.Event('diff.patternlock', {diffPassword: password});
         this.$container.trigger(diffEvent);
       // 密码一致
       } else {
-        var setEvent = $.Event('set.patternlock', {password: password});
+        let setEvent = $.Event('set.patternlock', {password: password});
         this.$container.trigger(setEvent);
         this.password = password;
       }
@@ -313,15 +304,16 @@
     } else if (this.options.mode === 'validate') {
       if (!this.password) throw new Error('No password set');
       if (password !== this.password) {
-        var wrongEvent = $.Event('wrong.patternlock', {wrongPassword: password});
+        let wrongEvent = $.Event('wrong.patternlock', {wrongPassword: password});
         this.$container.trigger(wrongEvent);
       } else {
-        var correctEvent = $.Event('correct.patternlock');
+        let correctEvent = $.Event('correct.patternlock');
         this.$container.trigger(correctEvent);
       }
     }
 
     // 重置组件状态
+    this.searchList = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     this.result = [];
     // 去掉橡皮筋
     this.elasticCtx.clearRect(0, 0, this.width, this.height);
@@ -372,7 +364,7 @@
 
   function Plugin (option) {
     return this.each(function () {
-      var $this   = $(this),
+      let $this   = $(this),
           data    = $this.data('patternLock'),
           options = $.extend({}, PatternLock.DEFAULTS, $this.data(), typeof option == 'object' && option);
 
@@ -384,7 +376,7 @@
   // 命名冲突处理
   // =================
 
-  var old = $.fn.patternLock;
+  let old = $.fn.patternLock;
 
   $.fn.patternLock = Plugin;
   $.fn.patternLock.Constructor = PatternLock;
